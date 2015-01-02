a46698c6a25b7593fcbda0f2d07fd1aa41b3b559
Frank Wang <eternnoir@gmail.com>
2015-01-02 15:07:47 +0800

    Format style.

--- src/NLogging/Formatter.cs
+++ src/NLogging/Formatter.cs
@@ -1,11 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace NLogging
+﻿namespace NLogging
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
     class Formatter
     {
     }
