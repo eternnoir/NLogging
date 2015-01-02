d74d63c49c0f858267f157c5e88b45758ee4e48b
Frank Wang <eternnoir@gmail.com>
2015-01-02 14:16:20 +0800

    Add some class.

--- src/NLogging/Logging.cs
+++ src/NLogging/Logging.cs
@@ -4,9 +4,29 @@ namespace NLogging
 {
 	public class Logging
 	{
-		public Logging ()
-		{
+
+             private static volatile Logging instance;
+   private static object syncRoot = new Object();
+
+   private Logging() {}
+
+   public static Logging Instance
+   {
+      get 
+      {
+         if (instance == null) 
+         {
+            lock (syncRoot) 
+            {
+               if (instance == null) 
+                  instance = new Logging();
+            }
+         }
+
+         return instance;
+      }
+   }
 		}
-	}
+	
 }
 
