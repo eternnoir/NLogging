dce279f0028c053291f5d3a89744017c18d029c2
Frank Wang <eternnoir@gmail.com>
2015-01-02 15:27:14 +0800

    Add log level to Logger.

--- src/NLogging/Logger.cs
+++ src/NLogging/Logger.cs
@@ -9,6 +9,7 @@
     public class Logger
     {
         private string loggerName;
+        private LogLevel logLevel;
 
         public string LoggerName 
         {
@@ -17,9 +18,32 @@
                 return this.loggerName;
             }
         }
+        
+        /// <summary>
+        /// Log level property
+        /// </summary>
+        public LogLevel Level
+        {
+            get
+            {
+                return this.logLevel;
+            }
+            set
+            {
+                this.logLevel = value;
+            }
+        }
+
         public Logger(string loggerName)
         {
             this.loggerName = loggerName;
+            this.logLevel = LogLevel.NOTSET;
+        }
+
+        public Logger(string loggerName, LogLevel logLevel)
+        {
+            this.loggerName = loggerName;
+            this.logLevel = logLevel;
         }
     }
 }
