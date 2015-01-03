d108eec1491fcf642ecdf0e45dc814d8c65397b9
Frank Wang <eternnoir@gmail.com>
2015-01-03 23:14:25 +0800

    Add testing project.

---
+++ src/NLogging.Test/LoggetTest.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NLogging;
+
+namespace NLogging.Test
+{
+    [TestFixture]
+    class LoggetTest
+    {
+        [Test]
+        public void TestLoggerName()
+        {
+            var loggerName ="TestLogger1"; 
+            var logger =  Logging.Instance.GetLogger(loggerName);
+            Assert.AreEqual(loggerName, logger.Name);
+        }
+    }
+}
