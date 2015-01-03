a8eb32f19d539e84f33c788610a21eab74e0287e
Frank Wang <eternnoir@gmail.com>
2015-01-03 20:00:00 +0800

    Add Record class.

---
+++ src/NLogging/Record.cs
@@ -0,0 +1,67 @@
+﻿namespace NLogging
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Linq;
+    using System.Text;
+
+    public class Record
+    {
+        private string loggerName;
+        private LogLevel level;
+        private StackTrace stack;
+        private string msg;
+        private string func;
+
+        public string LoggerName
+        {
+            get
+            {
+                return this.loggerName;
+            }
+        }
+
+        public LogLevel Level
+        {
+            get
+            {
+                return this.level;
+            }
+        }
+
+        public StackTrace Stack
+        {
+            get
+            {
+                return this.stack;
+            }
+        }
+
+        public string Message
+        {
+            get
+            {
+                return this.msg;
+            }
+        }
+
+        public string FunctionName
+        {
+            get
+            {
+                return this.func;
+            }
+        }
+
+        public Record(string loggerName, LogLevel logLevel, StackTrace stacktrace, string msg, string func)
+        {
+            this.loggerName = loggerName;
+            this.level = logLevel;
+            this.stack = stacktrace;
+            this.msg = msg;
+            this.func = func;
+        }
+
+    }
+}
