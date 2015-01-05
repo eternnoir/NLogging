7bce71b8f109c3daa09604931468f362787d8c4e
eternnoir <eternnoir@gmail.com>
2015-01-05 15:45:22 +0800

    Remove set log level from base handler class.

--- src/NLogging/Handler.cs
+++ src/NLogging/Handler.cs
@@ -9,24 +9,18 @@
     public abstract class Handler : IHandler
     {
         private IFormatter formatter;
-        private LogLevel logLevel;
 
         protected Handler()
         {
             this.formatter = new Formatter();
-            this.logLevel = LogLevel.NOTSET;
         }
 
         abstract public void push(Record record);
         abstract public void flush();
+
         public void SetFormatter(IFormatter formatter)
         {
             this.formatter = formatter;
-        }
-
-        public void SetLogLevel(LogLevel level)
-        {
-            this.logLevel = level;
         }
 
     }
