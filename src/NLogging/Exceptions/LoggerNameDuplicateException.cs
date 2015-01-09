a4fb27ded5d9cbe0350ea3b4bdc2509b445c8ea4
Frank Wang <eternnoir@gmail.com>
2015-01-09 15:27:41 +0800

    Readd file.

---
+++ src/NLogging/Exceptions/LoggerNameDuplicateException.cs
@@ -0,0 +1,13 @@
+﻿namespace NLogging.Exceptions
+{
+    public class LoggerNameDuplicateException : NLoggingException
+    {
+        public string LoggerName { get; set; }
+
+        public LoggerNameDuplicateException(string msg, string loggerName)
+            : base(msg)
+        {
+            this.LoggerName = loggerName;
+        }
+    }
+}
